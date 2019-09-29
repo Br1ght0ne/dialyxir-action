bbc36d1b1d327a7ce66748a34172a7fde440294d
Oleksii Filonenko <brightone@protonmail.com>
2019-09-29 20:48:50 +0300

    mix new

---
+++ lib/dialixyr_action.ex
@@ -0,0 +1,18 @@
+defmodule DialixyrAction do
+  @moduledoc """
+  Documentation for DialixyrAction.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DialixyrAction.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dialixyr_action_test.exs
@@ -0,0 +1,8 @@
+defmodule DialixyrActionTest do
+  use ExUnit.Case
+  doctest DialixyrAction
+
+  test "greets the world" do
+    assert DialixyrAction.hello() == :world
+  end
+end
